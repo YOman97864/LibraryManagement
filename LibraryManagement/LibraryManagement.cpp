558d1252ac187a385828194be64e361dd0f958a3
Ilya <osai9755@gmail.com>
2025-02-27 19:15:37 +0200

    loadBooks

--- LibraryManagement/LibraryManagement.cpp
+++ LibraryManagement/LibraryManagement.cpp
@@ -19,7 +19,16 @@ struct Book {
 vector<Book> books;
 int nextId = 1;
 
-
+void loadBooks() {
+    ifstream file("books.txt");
+    if (!file) return;
+    Book b;
+    while (file >> b.id >> ws && getline(file, b.title) && getline(file, b.author) && getline(file, b.genre) && file >> b.year >> b.available >> b.rating) {
+        books.push_back(b);
+        nextId = max(nextId, b.id + 1);
+    }
+    file.close();
+}
 
 
 
