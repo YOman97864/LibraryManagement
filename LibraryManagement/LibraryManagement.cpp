fb8142c470d14dd1773e6b819f9637223e91f43a
Ilya <osai9755@gmail.com>
2025-02-27 19:10:32 +0200

    first commit

--- LibraryManagement/LibraryManagement.cpp
+++ LibraryManagement/LibraryManagement.cpp
@@ -1,1 +1,22 @@
-﻿
+﻿#include <iostream>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <algorithm>
+
+using namespace std;
+
+
+
+
+
+
+
+
+
+
+
+int main() {
+    
+    return 0;
+}
